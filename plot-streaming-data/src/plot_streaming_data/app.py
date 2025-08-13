8d3f61918c0372067e55bcfdf94f872506d80d4d
Jose <jacbj42@gmail.com>
2025-08-13 17:52:20 +0200

    Add documentation

--- plot-streaming-data/src/plot_streaming_data/app.py
+++ plot-streaming-data/src/plot_streaming_data/app.py
@@ -12,18 +12,34 @@ import streamlit as st
 import plotly.graph_objects as go
 
 def split_csv_file_into_streaming_directory(file_path: Path, streaming_directory: Path):
+    """
+    Splits a CSV file into multiple single-row CSV files in a specified directory.
+    :param file_path: Path to the input CSV file to be split.
+    :type file_path: Path
+    :param streaming_directory: Directory where the split CSV files will be saved.
+    :type streaming_directory: Path
+    """
+    
+    MAX_FILES = 5000
     with open(file=file_path, mode='r') as file_:
         os.makedirs(name=streaming_directory, exist_ok=True)
         header = file_.readline()
         for i, line in enumerate(file_):
-            if (i != 0) or (len(line) > 1):
+            if (i <= MAX_FILES):
                 path = f'{streaming_directory}/ys_{i:06d}.csv'
                 with open(file=path, mode='w') as f:
                     f.write(header)
                     f.write(line)
 
 def initialise_streaming():
-        # 1. Crear sesión de Spark
+    """
+    Initializes a Spark session to simulate streaming data processing.
+    This function sets up a local Spark session, defines a schema for the input data,
+    reads CSV files in streaming mode from the 'data/file_sink/' directory, and writes
+    the results to the console in continuous update mode.
+    :raises pyspark.sql.utils.StreamingQueryException: If an error occurs during streaming execution.
+    """
+
     spark = SparkSession.builder \
         .appName("Simulate Streaming") \
         .master("local[8]") \
@@ -31,18 +47,16 @@ def initialise_streaming():
     
     spark.sparkContext.setLogLevel("ERROR")
 
-    # 2. Definir el esquema de los archivos CSV
     schema = StructType([
         StructField("id", IntegerType(), True),
-        StructField("y_pred", FloatType(), True),
-        StructField("y_test", FloatType(), True)
+        StructField("y_test", FloatType(), True),
+        StructField("y_pred", FloatType(), True)
     ])
 
-    # 3. Leer los archivos de la carpeta como stream
     df = spark.readStream \
         .option("header", True) \
         .schema(schema) \
-        .csv("stream_data/file_sink/")  # carpeta, no archivo individual
+        .csv("data/file_sink/")
     
     query = df \
         .writeStream \
@@ -53,49 +67,65 @@ def initialise_streaming():
     query.awaitTermination()
 
 def plot_data_plotly():
+    """
+    Visualizes in real time the comparison between predicted and actual traffic values using Plotly and Streamlit.
+    This function iterates over CSV files generated in real time, extracts prediction and actual values, and displays them in an interactive plot.
+    It detects anomalies when the difference between the predicted and actual value exceeds a threshold (20% of the actual value), highlighting them in the plot and showing a warning in the interface.
+    Processed files are deleted after visualization.
+    :raises Exception: Displays an error message in Streamlit if any issue occurs while processing the files.
+    """
 
-    st.title("Predicción vs Real - Simulación de Streaming (Plotly)")
+    st.title("Predicción vs Real - Streaming")
 
+    SLEEP = 1
+
+    x_vals_list = []
     y_pred_list = []
     y_test_list = []
-    x_vals = []
+    y_pred_colors = []
 
-    # Número total de archivos (puedes usar len(y_pred) también si ya lo tienes)
-    total_frames = len([f for f in os.listdir('stream_data/file_sink') if f.endswith('.csv')])
-
-    # Inicializar figura
-    fig = go.Figure()
-    line1 = go.Scatter(x=[], y=[], mode='lines+markers', name='y_pred', line=dict(color='blue'))
-    line2 = go.Scatter(x=[], y=[], mode='lines+markers', name='y_test', line=dict(color='green'))
-    fig.add_trace(line1)
-    fig.add_trace(line2)
+    total_frames = len([f for f in os.listdir('data/file_sink') if f.endswith('.csv')])
 
     plotly_chart = st.empty()
 
     for i in range(total_frames):
         try:
-            path = f'stream_data/file_sink/ys_{i:05d}.csv'
+            path = f'data/file_sink/ys_{i:06d}.csv'
             if os.path.exists(path):
                 df = pd.read_csv(path)
-                x_vals.append(int(df.loc[0, 'id']))
-                y_pred_list.append(float(df.loc[0, 'y_pred']))
-                y_test_list.append(float(df.loc[0, 'y_test']))
-                os.remove(path)
+                x_vals = int(df.loc[0, 'id'])
+                y_pred_value = float(df.loc[0, 'y_pred'])
+                y_test_value = float(df.loc[0, 'y_test'])
 
-                fig.data[0].x = x_vals
-                fig.data[0].y = y_pred_list
-                fig.data[1].x = x_vals
-                fig.data[1].y = y_test_list
+                x_vals_list.append(x_vals)
+                y_pred_list.append(y_pred_value)
+                y_test_list.append(y_test_value)
+                
+                THRESHOLD = 0.2 * y_test_value
+                
+                is_anomaly = abs(y_pred_value - y_test_value) > THRESHOLD
+                if is_anomaly:
+                    st.warning(body=f'ANOMALY: The value of the load of traffic was supossed to be: {y_test_value}, but {y_pred_value} has been predicted', icon='⚠️')
+                    y_pred_colors.append('red')
+                else:
+                    y_pred_colors.append('blue')
+
+                fig = go.Figure()
+                fig.add_traces([
+                    go.Scatter(x=x_vals_list, y=y_pred_list, mode='lines+markers', name='y_pred', line=dict(color='blue'), marker=dict(color=y_pred_colors)),
+                    go.Scatter(x=x_vals_list, y=y_test_list, mode='lines+markers', name='y_test', line=dict(color='green'))
+                ])
 
                 fig.update_layout(
-                    xaxis_title="Muestra",
-                    yaxis_title="Valor",
-                    title="Predicción vs Real (Streaming)",
-                    showlegend=True
+                xaxis_title="Muestra",
+                yaxis_title="Valor",
+                title="Predicción vs Real (Streaming)",
+                showlegend=True
                 )
 
-                plotly_chart.plotly_chart(fig, use_container_width=True)
-                time.sleep(0.5)  # Simula la llegada de datos
+                plotly_chart.plotly_chart(fig, use_container_width=True, key=f'dinamyc_plot_{i}')
+                os.remove(path)
+                time.sleep(SLEEP)
 
         except Exception as e:
             st.error(f"Error al procesar el archivo: {e}")
@@ -103,8 +133,8 @@ def plot_data_plotly():
 
 def main():
     split_csv_file_into_streaming_directory(
-        fetch_path=Path('stream_data/ys.csv'),
-        streaming_path=Path('stream_data/file_sink')
+        file_path=Path('data/test/ys_test.csv'),
+        streaming_directory=Path('data/file_sink')
     )
 
     threading.Thread(target=initialise_streaming, daemon=True).start()
