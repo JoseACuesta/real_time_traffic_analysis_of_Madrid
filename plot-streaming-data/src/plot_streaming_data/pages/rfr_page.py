b5627d770f1a43a6b64f70f7112f95eed91e9661
Jose <jacbj42@gmail.com>
2025-08-19 13:22:43 +0200

    Make ui more user-friendly

---
+++ plot-streaming-data/src/plot_streaming_data/pages/rfr_page.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from pathlib import Path
+import threading
+
+from plot_streaming_data.utils.split_csv_files import split_rfr_prediction_data_file_into_streaming_directory
+
+from plot_streaming_data.utils.spark_client import initialise_rfr_streaming
+
+from plot_streaming_data.utils.plot_real_time_data import plot_rfr_data_plotly
+
+split_rfr_prediction_data_file_into_streaming_directory(
+    file_path=Path('data/RandomForestRegressor/test/ys_test.csv'),
+    streaming_directory=Path('data/RandomForestRegressor/file_sink')
+)
+
+threading.Thread(
+    target=initialise_rfr_streaming, daemon=True
+).start()
+
+plot_rfr_data_plotly()
---
+++ plot-streaming-data/src/plot_streaming_data/utils/plot_real_time_data.py
@@ -0,0 +1,135 @@
+import pandas as pd
+
+import os
+import time
+
+import streamlit as st
+import plotly.graph_objects as go
+
+def plot_rfr_data_plotly():
+    """
+    Visualizes in real time the comparison between predicted and actual traffic values using Plotly and Streamlit.
+    This function iterates over CSV files generated in real time, extracts prediction and actual values, and displays them in an interactive plot.
+    It detects anomalies when the difference between the predicted and actual value exceeds a threshold (20% of the actual value), highlighting them in the plot and showing a warning in the interface.
+    Processed files are deleted after visualization.
+    :raises Exception: Displays an error message in Streamlit if any issue occurs while processing the files.
+    """
+
+    SLEEP = 1
+
+    x_vals_list = []
+    y_pred_list = []
+    y_test_list = []
+    y_pred_colors = []
+
+    total_frames = len([f for f in os.listdir('data/RandomForestRegressor/file_sink') if f.endswith('.csv')])
+
+    plotly_chart = st.empty()
+    warning_container = st.container(border=True, height=400)
+
+    for i in range(total_frames):
+        try:
+            path = f'data/RandomForestRegressor/file_sink/ys_{i:06d}.csv'
+            if os.path.exists(path):
+                df = pd.read_csv(path)
+                x_vals = int(df.loc[0, 'id'])
+                y_pred_value = float(df.loc[0, 'y_pred'])
+                y_test_value = float(df.loc[0, 'y_test'])
+
+                x_vals_list.append(x_vals)
+                y_pred_list.append(y_pred_value)
+                y_test_list.append(y_test_value)
+                
+                THRESHOLD = 0.2 * y_test_value
+                
+                is_anomaly = abs(y_pred_value - y_test_value) > THRESHOLD
+                if is_anomaly:
+                    warning_container.warning(body=f'ANOMALY: The value of the load of traffic was supossed to be: {y_test_value}, but {y_pred_value} has been predicted', icon='⚠️')
+                    y_pred_colors.append('red')
+                else:
+                    y_pred_colors.append('blue')
+
+                fig = go.Figure()
+                fig.add_traces([
+                    go.Scatter(x=x_vals_list, y=y_pred_list, mode='lines+markers', name='y_pred', line=dict(color='blue'), marker=dict(color=y_pred_colors)),
+                    go.Scatter(x=x_vals_list, y=y_test_list, mode='lines+markers', name='y_test', line=dict(color='green'))
+                ])
+
+                fig.update_layout(
+                xaxis_title="Muestra",
+                yaxis_title="Valor",
+                title="Predicción vs Real (Streaming)",
+                showlegend=True
+                )
+
+                plotly_chart.plotly_chart(fig, use_container_width=False, key=f'dinamyc_plot_{i}')
+                os.remove(path)
+                time.sleep(SLEEP)
+
+        except Exception as e:
+            st.error(f"Error al procesar el archivo: {e}")
+            continue
+
+def plot_xgboost_data_plotly():
+    """
+    Visualizes in real time the comparison between predicted and actual traffic values using Plotly and Streamlit.
+    This function iterates over CSV files generated in real time, extracts prediction and actual values, and displays them in an interactive plot.
+    It detects anomalies when the difference between the predicted and actual value exceeds a threshold (20% of the actual value), highlighting them in the plot and showing a warning in the interface.
+    Processed files are deleted after visualization.
+    :raises Exception: Displays an error message in Streamlit if any issue occurs while processing the files.
+    """
+
+    SLEEP = 1
+
+    x_vals_list = []
+    y_pred_list = []
+    y_test_list = []
+    y_pred_colors = []
+
+    total_frames = len([f for f in os.listdir('data/XGBoost/file_sink') if f.endswith('.csv')])
+
+    plotly_chart = st.empty()
+    warning_container = st.container(border=True, height=200)
+
+    for i in range(total_frames):
+        try:
+            path = f'data/XGBoost/file_sink/ys_{i:06d}.csv'
+            if os.path.exists(path):
+                df = pd.read_csv(path)
+                x_vals = int(df.loc[0, 'id'])
+                y_pred_value = float(df.loc[0, 'y_pred'])
+                y_test_value = float(df.loc[0, 'y_test'])
+
+                x_vals_list.append(x_vals)
+                y_pred_list.append(y_pred_value)
+                y_test_list.append(y_test_value)
+                
+                THRESHOLD = 0.2 * y_test_value
+                
+                is_anomaly = abs(y_pred_value - y_test_value) > THRESHOLD
+                if is_anomaly:
+                    warning_container.warning(body=f'ANOMALY: The value of the load of traffic was supossed to be: {y_test_value}, but {y_pred_value} has been predicted', icon='⚠️')
+                    y_pred_colors.append('red')
+                else:
+                    y_pred_colors.append('blue')
+
+                fig = go.Figure()
+                fig.add_traces([
+                    go.Scatter(x=x_vals_list, y=y_pred_list, mode='lines+markers', name='y_pred', line=dict(color='blue'), marker=dict(color=y_pred_colors)),
+                    go.Scatter(x=x_vals_list, y=y_test_list, mode='lines+markers', name='y_test', line=dict(color='green'))
+                ])
+
+                fig.update_layout(
+                xaxis_title="Muestra",
+                yaxis_title="Valor",
+                title="Predicción vs Real (Streaming)",
+                showlegend=True
+                )
+
+                plotly_chart.plotly_chart(fig, use_container_width=True, key=f'dinamyc_plot_{i}')
+                os.remove(path)
+                time.sleep(SLEEP)
+
+        except Exception as e:
+            st.error(f"Error al procesar el archivo: {e}")
+            continue
